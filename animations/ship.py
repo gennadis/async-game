13d9f5632f1fbbb3ad2609e9ef6220393c50b6e7
Gennady Badinov <gennady.badinov@gmail.com>
2022-06-03 15:23:54 +0300

    Refactor spaceship flying methods

--- animations/ship.py
+++ animations/ship.py
@@ -20,6 +20,26 @@ def load_frames(
     return frame_1, frame_2
 
 
+def calculate_coordinate(
+    current_coordinate: int,
+    max_coordinate: int,
+    direction: int,
+    frame_size: int,
+):
+    # top and left screen sides handling
+    if current_coordinate <= 1 and direction == -1:
+        next_coordinate = 1
+
+    # bottom and right screen sides handling
+    elif max_coordinate - (current_coordinate + frame_size) <= direction:
+        next_coordinate = max_coordinate - frame_size - 1
+
+    else:
+        next_coordinate = current_coordinate + direction
+
+    return next_coordinate
+
+
 async def fly(
     canvas,
     row: int,
@@ -28,27 +48,22 @@ async def fly(
     frame_2: str,
     screen_height: int,
     screen_width: int,
-    border_offset: int = 1,
 ) -> None:
     frame_height, frame_width = get_frame_size(frame_1)
-    row, column = row + 1, column - frame_width // 2
-    available_movement_height = screen_height - frame_height - border_offset
-    available_movement_width = screen_width - frame_width - border_offset
 
     while True:
         for frame in cycle([frame_1, frame_2]):
             rows_direction, columns_direction, _ = read_controls(canvas)
-            row = (
-                min(row + rows_direction, available_movement_height)
-                if rows_direction > 0
-                else max(row + rows_direction, 1)
-            )
-            column = (
-                min(column + columns_direction, available_movement_width)
-                if columns_direction > 0
-                else max(column + columns_direction, 1)
-            )
+
+            if rows_direction:
+                row = calculate_coordinate(
+                    row, screen_height, rows_direction, frame_height
+                )
+            if columns_direction:
+                column = calculate_coordinate(
+                    column, screen_width, columns_direction, frame_width
+                )
 
             draw_frame(canvas, row, column, frame)
             await asyncio.sleep(0)
-            draw_frame(canvas, row, column, frame, negative=True)
+            draw_frame(canvas, row, column, frame, True)
